28ba5ece9465e2560d00760c8c70b4a2e2afa5e3
Ryan Allen <rallenwasd@gmail.com>
2020-04-07 09:23:43 -0500

    Replace argument body with notes

--- app/graphql/mutations/create_event.rb
+++ app/graphql/mutations/create_event.rb
@@ -1,6 +1,6 @@
 class Mutations::CreateEvent < Mutations::BaseMutation
   argument :title, String, required: true
-  argument :body, String, required: true
+  argument :notes, String, required: true
   argument :date, String, required: true
   argument :time, String, required: true
   argument :income, Float, required: true
@@ -10,9 +10,9 @@ class Mutations::CreateEvent < Mutations::BaseMutation
   field :event, Types::EventType, null: false
   field :errors, [String], null: false
 
-  def resolve(title:, body:, date:, time:, income:, mileage:, user_id:)
+  def resolve(title:, notes:, date:, time:, income:, mileage:, user_id:)
     user = User.find(user_id)
-    event = user.events.new(title: title, body: body, date: date, time: time,
+    event = user.events.new(title: title, notes: notes, date: date, time: time,
                     income: income, mileage: mileage, user_id: user_id)
     if event.save
       # Successful creation, return the created object with no errors
