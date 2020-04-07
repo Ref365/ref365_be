4c3e1e1e3e20450f6dc342e7bd6090cff9ac6673
Ryan Allen <rallenwasd@gmail.com>
2020-04-07 09:23:57 -0500

    Replace field body with notes

--- app/graphql/types/event_type.rb
+++ app/graphql/types/event_type.rb
@@ -2,7 +2,7 @@ module Types
   class EventType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: true
-    field :body, String, null: true
+    field :notes, String, null: true
     field :date, String, null: true
     field :time, String, null: true
     field :mileage, Float, null: true
