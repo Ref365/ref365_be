86e227e7f543fc5a0854a82fcb9f77ae3eaa01a4
Melissa Robbins <melissamrobbins@gmail.com>
2020-04-08 09:01:07 -0600

    Update js config for GraphiQL

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link graphiql/rails/application.css
 //= link graphiql/rails/application.js
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
