a4ffafc99cbf87394275b5fd8ce07f7beba967c7
Ryan Allen <rallenwasd@gmail.com>
2020-04-08 15:41:38 -0500

    Update root page with links to github repositories and the back-end readme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    render json: "Hello AWS! -REF365"
-  end
+    render json: %q{
+      Welcome to the REF365 Back-end Application.
+
+      Visit our github page at: https://github.com/Ref365
+
+      View our readme at: https://github.com/Ref365/ref365_be/blob/master/README.md
+
+      Thanks.
+      
+       - REF365 Team
+    }end
 end
