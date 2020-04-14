c229a09de3a56a0a3e01af9f1b9cb43b2c01be1b
Ryan Allen <rallenwasd@gmail.com>
2020-04-14 11:48:31 -0500

    Add mutation type delete event

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_event, mutation: Mutations::CreateEvent
+    field :delete_event, mutation: Mutations::DeleteEvent
   end
 end
